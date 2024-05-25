741f7bbee376b8b753e50bad03691caaad4fa999
Gabe <28gabe@gmail.com>
2024-05-24 19:18:05 -0700

    removes something

--- app/[page]/page.tsx
+++ app/[page]/page.tsx
@@ -1,27 +1,5 @@
-import type { Metadata } from 'next';
-
 import { getPage } from 'lib/shopify';
 import { notFound } from 'next/navigation';
-
-export async function generateMetadata({
-  params
-}: {
-  params: { page: string };
-}): Promise<Metadata> {
-  const page = await getPage(params.page);
-
-  if (!page) return notFound();
-
-  return {
-    title: page.seo?.title || page.title,
-    description: page.seo?.description || page.bodySummary,
-    openGraph: {
-      publishedTime: page.createdAt,
-      modifiedTime: page.updatedAt,
-      type: 'article'
-    }
-  };
-}
 
 export default async function Page({ params }: { params: { page: string } }) {
   const page = await getPage(params.page);
