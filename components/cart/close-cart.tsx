e57bdb0d0361216785d816e18c0b9982456b8923
Gabe <28gabe@gmail.com>
2024-06-30 19:22:47 -0700

    cleans cart component

--- components/cart/close-cart.tsx
+++
@@ -1,7 +0,0 @@
-export default function CloseCart({ className }: { className?: string }) {
-  return (
-    <div className="relative flex h-11 w-11 items-center justify-center rounded-md border border-neutral-200 text-black transition-colors dark:border-neutral-700 dark:text-white">
-      x
-    </div>
-  );
-}
