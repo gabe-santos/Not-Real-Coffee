04818f1718881e8e7c86f4ab0583463103110a06
Gabe <28gabe@gmail.com>
2024-07-03 15:50:45 -0700

    replaces logo text with svg

--- components/navigation/navbar.tsx
+++ components/navigation/navbar.tsx
@@ -38,13 +38,10 @@ export default function Navbar() {
   return (
     <nav className="fixed z-50 flex h-2xl w-full max-w-screen items-center justify-between px-sm py-sm">
       <Link href="/" className="flex items-center bg-glass pill">
-        <span className="text-lg font-semibold tracking-tight">NOT REAL*</span>
-        {/* <Image src={logo} alt="logo" className="h-[34px]" /> */}
+        <Image src={logo} alt="logo" className="h-[34px] w-fit" />
       </Link>
-      {/* <div className="flex items-center justify-end gap-xs"> */}
       <NavMenu options={navList} />
       <Cart />
-      {/* </div> */}
     </nav>
   );
 }
